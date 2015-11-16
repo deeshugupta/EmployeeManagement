3f125f847fc4759d1502105998bdff7159b69693
Manoj <manoj.delhi24@gmail.com>
2015-11-16 17:57:36 +0530

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-LeaveType.create(:name => "Casual")
-LeaveType.create(:name => "Sick")
-LeaveType.create(:name => "Privilege")
+if LeaveType.count == 0
+  LeaveType.create(:name => "Casual")
+  LeaveType.create(:name => "Sick")
+  LeaveType.create(:name => "Privilege")
+end
+
+if Role.count == 0
+  [ {"working_days"=>nil, "name"=>"admin", "leaves_applicable"=>nil},
+    {"working_days"=>5, "name"=>"Developer", "leaves_applicable"=>nil},
+    {"working_days"=>6, "name"=>"Manager", "leaves_applicable"=>nil},
+    {"working_days"=>5, "name"=>"QA", "leaves_applicable"=>nil},
+    {"working_days"=>6, "name"=>"Designer", "leaves_applicable"=>nil}
+  ].each do |attrs|
+    Role.create attrs
+  end
+end
+
+if User.count == 0
+  user = User.create email: 'admin@test.com', password: 'password', password_confirmation: 'password'
+  user.role << Role.first
+end
