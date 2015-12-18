169c71577d0ae545e510410c9ebc8831f124e5a6
nd-rordev <naresh.dwivedi@traveltriangle.com>
2015-12-18 10:45:56 +0530

    Removed test seeds from seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,13 @@ if admin.nil?
   admin = User.create email: 'admin@traveltriangle.com', password: 'password', password_confirmation: 'password'
   admin.roles << Role.admin
   admin.roles << Role.manager
+else
+  if !admin.is_admin?
+    admin.roles << Role.admin
+  end
+  if !admin.is_manager?
+    admin.roles << Role.manager
+  end
 end
 
 # checking and creating developer main manager user
@@ -36,31 +43,35 @@ dev_manager = User.where(:email => "dev@traveltriangle.com").first
 if dev_manager.nil?
   dev_manager = User.create email: 'dev@traveltriangle.com', password: 'password', password_confirmation: 'password', manager_id: admin.id
   dev_manager.roles << Role.manager
+else
+  if !dev_manager.is_manager?
+    dev_manager.roles << Role.manager
+  end
 end
 
 # creating test managers
-(1..5).each do |i|
-  if User.where(:email => "manager+#{i}@traveltriangle.com").first.nil?
-    user = User.create email: "manager+#{i}@traveltriangle.com", password: 'password', password_confirmation: 'password', name: "Manager #{i}", manager_id: dev_manager.id
-    user.roles << Role.manager
-  end
-end
+# (1..5).each do |i|
+#   if User.where(:email => "manager+#{i}@traveltriangle.com").first.nil?
+#     user = User.create email: "manager+#{i}@traveltriangle.com", password: 'password', password_confirmation: 'password', name: "Manager #{i}", manager_id: dev_manager.id
+#     user.roles << Role.manager
+#   end
+# end
 
 # creating test developers for each above manager
-Role.manager.users.each_with_index do |manager_user, i|
-  if manager_user.id != admin.id && manager_user.id != dev_manager.id
-    ((i * 40)..((i + 1) * 40)).each do |i|
-      if User.where(:email => "developer+#{i}@traveltriangle.com").first.nil?
-        user = User.create email: "developer+#{i}@traveltriangle.com", password: 'password', password_confirmation: 'password', name: "Developer #{i}", manager_id: manager_user.id
-        user.roles << Role.developer
-      end
-    end
-  end
-end
+# Role.manager.users.each_with_index do |manager_user, i|
+#   if manager_user.id != admin.id && manager_user.id != dev_manager.id
+#     ((i * 40)..((i + 1) * 40)).each do |i|
+#       if User.where(:email => "developer+#{i}@traveltriangle.com").first.nil?
+#         user = User.create email: "developer+#{i}@traveltriangle.com", password: 'password', password_confirmation: 'password', name: "Developer #{i}", manager_id: manager_user.id
+#         user.roles << Role.developer
+#       end
+#     end
+#   end
+# end
 
-if Holiday.where(:name => "Deepawali").first.nil?
-  Holiday.create(:name => "Deepawali", on_date: "2015-11-11", days: 2)
-end
+# if Holiday.where(:name => "Deepawali").first.nil?
+#   Holiday.create(:name => "Deepawali", on_date: "2015-11-11", days: 2)
+# end
 
 
 
